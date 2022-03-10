c7f19b5c03a7cef25bb3e31f1323875cb27cf187
Gustavo0201 <gustavo0201tartari@gmail.com>
2022-03-10 20:04:12 -0300

    exe

---
+++ Facec.Exercicios.Lista.Dois-main/Lista.Dois.Exercicio-01/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Runtime.InteropServices.ComTypes;
+namespace Exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            try 
+            {
+                Console.WriteLine("Informe sua data de nascimento: ");
+                DateTime data = DateTime.Parse(Console.ReadLine());
+
+                int idade = DateTime.Today.Year - data.Year;
+                
+                if (data.DayOfYear < DateTime.Today.DayOfYear)
+                    idade = idade - 1;
+
+                Console.WriteLine($"Sua idade é de: {idade}");
+                
+
+                Console.WriteLine("Sua idade é de: " + 
+                                $"{(DateTime.Today.Year - data.Year)}"
+                                + " anos !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }   
+    
+}
---
+++ Facec.Exercicios.Lista.Dois-main/Lista.Dois.Exercicio-02/Program.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Exercicio_02
+{
+    internal class Program
+    {
+        private const string _sim = "SIM";
+        static void Main(string[] args)
+        {
+            string continuar = string.Empty;
+            char operacao = 'a';
+            decimal valor1, valor2 = 0.0m;
+            try
+            {
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Informe a operação:");
+                        operacao = char.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    switch (operacao)
+                    {
+                        case '+':
+                            Console.WriteLine("Informe o primeiro valor para a soma:");
+                            valor1 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Informe o primeiro valor para a soma:");
+                            valor2 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine($"O resultado da sua conta é:{valor1 + valor2}");
+                            break;
+                        case '-':
+                            Console.WriteLine("Informe o primeiro valor para a subtração:");
+                            valor1 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Informe o primeiro valor para a subtração:");
+                            valor2 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine($"O resultado da sua conta é:{valor1 - valor2}");
+                            break;
+                        case '*':
+                            Console.WriteLine("Informe o primeiro valor para a multiplicação:");
+                            valor1 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Informe o primeiro valor para a multiplicação:");
+                            valor2 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine($"O resultado da sua conta é:{valor1 * valor2}");
+                            break;
+                        case '/':
+                            Console.WriteLine("Informe o primeiro valor para a divisão:");
+                            valor1 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Informe o primeiro valor para a divisão:");
+                            valor2 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine($"O resultado da sua conta é:{valor1 / valor2}");
+                            break;
+                        case '%':
+                            Console.WriteLine("Informe o primeiro valor para o resto da divisão:");
+                            valor1 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Informe o primeiro valor para o resto da divisão::");
+                            valor2 = decimal.Parse(Console.ReadLine());
+
+                            Console.WriteLine($"O resultado da sua conta é:{valor1 % valor2}");
+                            break;
+                        default:
+                            throw new Exception("Operação matemática não " +
+                                        "configurada !");
+                    }
+
+                    Console.WriteLine("Deseja continuar ? [sim/nao]");
+                    continuar = Console.ReadLine();
+
+                } while (_sim.Equals(continuar.ToUpper()));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+        }
+
+    }
+}
